104066f08586d570d37c212ca2994682990ff3aa
Jago Van Dam <Jago.VanDam@sa.gov.au>
2022-11-16 10:47:06 +1030

    changed app to include buttons, removed .Rmd file

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,31 @@
-function setIframe() {
-    var selection = document.getElementById("fig_select").value;
-    document.getElementById("figure-display").src = selection
+function cod_setIframe() {
+  let selected = document.getElementById("fig_select");
+  let selectionID = selected.options[selected.selectedIndex].id;
+  let selection = document.getElementById("fig_select").value;
+  document.getElementById("figure-display").src = selection;
+  buttons = document.querySelector('#buttonBlock')
+  switch(selectionID) {
+    case 'figure3':
+      buttons.style.display = 'flex';
+      let subButtons = document.querySelectorAll("button.cod-sub-button");
+      subButtons.forEach(function(button) {
+        button.classList.remove('activeButton')
+      });
+      document.querySelector('button.cod-sub-button').classList.add('activeButton');
+      break;
+    default:
+      buttons.style.display = 'none';
+  }
 }
 
+function subButtonSetIframe(clickedButton) {
+    var subButtons = document.querySelectorAll("button.cod-sub-button");
+    subButtons.forEach(function(button) {
+      button.classList.remove('activeButton')
+    });
+    clickedButton.classList.add('activeButton');
+    document.getElementById("figure-display").src = clickedButton.value;
+}
 
 function openPanel() {
     var panel = document.getElementById("panel");
@@ -17,3 +40,4 @@ function openPanel() {
         panel.style.display = "block";
       }
 }
+
